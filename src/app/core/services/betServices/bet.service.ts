09a69ad2655a3cb4f8ec0b4a7d2d17c21161c493
gianca970226 <giancarlosfigueroa@hotmail.com>
2018-04-14 13:03:10 -0500

    Code
    Commin

---
+++ src/app/core/services/betServices/bet.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+
+import 'rxjs/add/operator/map';
+import {Credentials} from '../../model/credentials';
+
+
+@Injectable()
+export class SessionService {
+
+  private token;
+  private credentials;
+
+  setSession(token: string, credentials: Credentials) {
+    this.token = token;
+    this.credentials = credentials;
+  }
+
+  getSessionToken(): string {
+    return this.token;
+  }
+
+  getCredentials() {
+    return this.credentials;
+  }
+
+  isLogged(): boolean {
+    return !!this.token;
+  }
+
+}
