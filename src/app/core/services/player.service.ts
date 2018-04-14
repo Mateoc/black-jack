159fb8594a60943ebb254e07eabc3b2f33329c56
JoanSerna <jsernaleiton@gmail.com>
2018-04-14 13:01:40 -0500

    Joan Serna design login created and request finally

--- src/app/core/services/player.service.ts
+++ src/app/core/services/player.service.ts
@@ -1,27 +1,52 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
-import {SessionService} from './uthorization/session.service';
-import {Credentials} from '../model/credentials';
-import {Observable} from "rxjs/Observable";
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { SessionService } from './uthorization/session.service';
+import { Credentials } from '../model/credentials';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PlayerService {
-
   credentials: Credentials;
 
-  constructor(private http: HttpClient, private sessionService: SessionService) {
+  constructor(
+    private http: HttpClient,
+    private sessionService: SessionService
+  ) {
     this.credentials = sessionService.getCredentials();
   }
 
-  getPlayerState(): Observable<any>{
+  getPlayerState(): Observable<any> {
     return this.http.get(`${environment.api}/api/player/state`);
   }
 
-  getPlayer(){
+  getPlayer() {
     return this.http.get(`${environment.api}/api/player/`);
   }
+  getBet() {
+    return this.http
+      .post(`${environment.api}/api/test/player/bet`, {
+        toke: this.sessionService.getSessionToken(),
+        bet: {
+          value: 999
+        }
+      })
+      .toPromise();
+  }
 
+  getHit() {
+    return this.http
+      .post(`${environment.api}/api/test/player/hit`, {
+        token: this.sessionService.getSessionToken()
+      })
+      .toPromise();
+  }
+  getStand() {
+    return this.http
+      .post(`${environment.api}/api/test/player/stand`, {
+        token: this.sessionService.getSessionToken()
+      })
+      .toPromise();
+  }
 }
--- src/app/states/game/game.component.ts
+++ src/app/states/game/game.component.ts
@@ -1,10 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {GameStateService} from '../../core/services/game-state.service';
-import {Observable} from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
+import { GameStateService } from '../../core/services/game-state.service';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
-
-import {PlayerService} from '../../core/services/player.service';
+import { PlayerService } from '../../core/services/player.service';
 import Player from '../../core/model/player';
 
 @Component({
@@ -13,7 +12,6 @@ import Player from '../../core/model/player';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
   playerObservable: Observable<Player>;
   gameStateObservable: Observable<string>;
 
@@ -21,29 +19,32 @@ export class GameComponent implements OnInit {
     CREATED: 'Waiting for bets',
     PLAYERS_TURN: 'Dealing players',
     FINISHED: 'Round Finished',
-    DEALER_TURN: 'Dealer\'s turn'
+    DEALER_TURN: "Dealer's turn"
   };
 
-  constructor(private gameStateService: GameStateService, private playerService: PlayerService) {
-  }
+  constructor(
+    private gameStateService: GameStateService,
+    private playerService: PlayerService
+  ) {}
 
   ngOnInit() {
     this.playerObservable = this.gameStateService.userObserver();
-    this.gameStateObservable = this.gameStateService.gameStateObserver().map(response => {
-      return this.codes[response] || response;
-    });
+    this.gameStateObservable = this.gameStateService
+      .gameStateObserver()
+      .map(response => {
+        return this.codes[response] || response;
+      });
   }
 
   bet() {
-
+    this.playerService.getBet().then(res => console.log(res));
   }
 
   hit() {
-
+    this.playerService.getHit().then(res => console.log(res));
   }
 
   stand() {
-
+    this.playerService.getStand().then(resp => console.log(resp));
   }
-
 }
